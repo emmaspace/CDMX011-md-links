bfe82112188c87cf59c82a52d8755a68651cc4c1
Emma <emmaspace@users.noreply.github.com>
2021-10-05 12:32:49 -0500

    build: Check first argument (path)
    - Checks if the first argument is undefined.
    - Checks if it is a valid path.
    - Checks if it's absolute or relative.
    - Turns it into absolute if necessary.
    Closes #5 , #6 , #2

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = () => {
-  // ...
+const fs = require("fs");
+const path = require("path");
+const { cwd } = require("process");
+
+//let input = process.argv[2];
+
+module.exports = (input, callback) => {
+  // ---------------------------------
+  // Ver si esta parte la pongo en otra función
+  if (input === undefined) input = cwd();
+  if (path.isAbsolute(input) === false) input = path.resolve(input);
+  // ------------------------------------
+  fs.readdir(input, (err, content) => {
+    if (err && err["errno"] === -20) callback(null, [input]);
+    else if (err && err["errno"] === -2)
+      return console.log("There's no such file or directory");
+    else if (err) return console.log(err);
+    else callback(null, content);
+  });
 };
+
+/*getFiles(input, (err, content) => {
+  if (err) return console.log(err);
+  console.log(content);
+});*/
