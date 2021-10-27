9be79a4b3b41e973578ce31a636b902d3b56fe1a
Emma <emmarraguila@comunidad.unam.mx>
2021-10-27 09:07:22 -0500

    test: Add get-files test
    Also check output cases.
    Closes #30 , closes #21

---
+++ test/get-files.spec.js
@@ -0,0 +1,29 @@
+const getFiles = require("../lib/js-api/get-files");
+
+describe("getFiles", () => {
+  it("should be a function", () => {
+    expect(typeof getFiles).toBe("function");
+  });
+  const output = [
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/.DS_Store",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/Readme.md",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/Readme2.md",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/ReadmeData.md",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/chunches.js",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/.DS_Store",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/Readme-gym.md",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/Readme.md",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/Readme2.md",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/mas-chunches.js",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/prueba3/.DS_Store",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/prueba3/Readme.md",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/prueba3/ReadmeRed.md",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/prueba3/hola.js",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/prueba3/prueba4/Readme.md",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/prueba3/prueba4/Readme2.md",
+    "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/prueba/prueba2/prueba3/prueba4/ReadmeLab.md",
+  ];
+  it("should return all the files in the directory", () => {
+    expect(getFiles("prueba", "/Users/emma/Documents/Laboratoria/Proyectos/CDMX011-md-links/")).toEqual(output);
+  });
+});
